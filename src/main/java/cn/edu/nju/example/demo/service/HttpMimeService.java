209d456669f4d5e6470ced8d62b02606ae575da7
Mr Gao <619903532@qq.com>
2019-12-18 20:53:38 +0800

    feat: code and test the mime type of image/jpeg and gif

--- src/main/java/cn/edu/nju/example/demo/service/HttpMimeService.java
+++ src/main/java/cn/edu/nju/example/demo/service/HttpMimeService.java
@@ -1,12 +1,45 @@
 package cn.edu.nju.example.demo.service;
 
-import cn.edu.nju.nioserver.http.HttpRequest;
-import cn.edu.nju.nioserver.http.HttpResponse;
-import cn.edu.nju.nioserver.http.HttpService;
+import cn.edu.nju.nioserver.http.*;
+
+import java.awt.image.BufferedImage;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import sun.misc.BASE64Encoder;
+
+import sun.misc.BASE64Decoder;
+
+import javax.imageio.ImageIO;
 
 public class HttpMimeService implements HttpService {
+    static BASE64Encoder encoder = new sun.misc.BASE64Encoder();
+
     @Override
     public void service(HttpRequest request, HttpResponse response) {
-        // TODO gy
+        String path = request.uri();
+        String fileName = "src/main/resources/"+path;
+        String type = path.substring(1);
+        String pre = type.split("/")[0];
+        String post = type.split("\\.")[type.split("\\.").length - 1];
+      /*
+        尝试将文件转为二进制进行传输
+         String biImage = "";
+        try {
+            BufferedImage bi = ImageIO.read(new File(fileName));
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            ImageIO.write(bi, "jpg", baos);
+            byte[] bytes = baos.toByteArray();
+            biImage = encoder.encodeBuffer(bytes).trim();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }*/
+        StringBuilder responseBuilder = new StringBuilder();
+        responseBuilder.append("You have get url with " + path + " and ")
+                .append("the mime type of file is " + pre + "/" + post);
+//            .append(biImage);
+        //后面此处需要考虑如何设计，无法默认长度（可以是chunk的方式）
+        response.headers().set(HttpHeaderNames.CONTENT_LENGTH, "" + responseBuilder.toString().getBytes(StandardCharsets.UTF_8).length);
+        response.content().setString(responseBuilder.toString());
+
     }
 }
---
+++ src/test/java/cn/edu/nju/example/demo/service/HttpMimeServiceTest.java
@@ -0,0 +1,63 @@
+package cn.edu.nju.example.demo.service;
+
+import cn.edu.nju.nioserver.http.HttpRequest;
+import cn.edu.nju.nioserver.http.HttpRequestDecoder;
+import cn.edu.nju.nioserver.http.HttpResponse;
+import cn.edu.nju.nioserver.http.HttpService;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * DemoMimeService Tester.
+ * @author 61990
+ */
+public class HttpMimeServiceTest {
+
+    @Before
+    public void before() {
+    }
+
+    @After
+    public void after() {
+    }
+
+
+    /**
+     * Method: service(HttpRequest request, HttpResponse response)
+     */
+    @Test
+    public void testService1() {
+        String httpRequest =
+                "GET /image/hello.jpg HTTP/1.1\r\n" +
+                        "Host:www.hostname.com\r\n" +
+                        "User-Agent:Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; .NET CLR 2.0.50727; .NET CLR 3.0.04506.648; .NET CLR 3.5.21022)\r\n" +
+                        "Content-Type:application/x-www-form-urlencoded\r\n" +
+                        "Content-Length:40\r\n" +
+                        "Connection: Keep-Alive\r\n" +
+                        "\r\n" +
+                        "name=Professional%20Ajax&publisher=Wiley";
+
+        byte[] source = httpRequest.getBytes(StandardCharsets.UTF_8);
+        ByteBuffer byteBuffer = ByteBuffer.allocate(source.length);
+        byteBuffer.put(source);
+        List<HttpRequest> requestList = new ArrayList<>();
+        HttpRequestDecoder decoder = new HttpRequestDecoder();
+        decoder.decode(0, byteBuffer, requestList);
+
+        HttpRequest request = requestList.get(0);
+        HttpService service = new HttpMimeService();
+        HttpResponse response = new HttpResponse();
+        service.service(request, response);
+        String result = new String(response.content().byteBuffer().array(), StandardCharsets.UTF_8);
+        assertEquals(result, "You have get url with " +request.uri()+" and the mime type of file is image/jpg");
+    }
+
+} 
