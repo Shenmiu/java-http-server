054c14652960db97a6522afa211c5cb88836f0f2
CharlesFeng <fdfengjunjie@126.com>
2019-12-21 19:14:40 +0800

    fix: 不支持请求方法时，服务器应当返回状态码405（Method Not Allowed）

--- src/main/java/cn/edu/nju/example/demo/exception/NotSupportedHttpMethodException.java
+++
@@ -1,9 +0,0 @@
-package cn.edu.nju.example.demo.exception;
-
-public class NotSupportedHttpMethodException extends Exception {
-
-    @Override
-    public String getMessage() {
-        return "Not supported http method";
-    }
-}
--- src/main/java/cn/edu/nju/example/demo/service/method/HttpMethodServiceNotSupported.java
+++ src/main/java/cn/edu/nju/example/demo/service/method/HttpMethodServiceNotSupported.java
@@ -2,13 +2,13 @@ package cn.edu.nju.example.demo.service.method;
 
 import cn.edu.nju.nioserver.http.HttpRequest;
 import cn.edu.nju.nioserver.http.HttpResponse;
+import cn.edu.nju.nioserver.http.HttpResponseStatus;
 
 public class HttpMethodServiceNotSupported implements HttpMethodServiceInt {
     @Override
     public boolean process(HttpRequest request, HttpResponse response) {
-        StringBuilder responseBuilder = new StringBuilder();
-        responseBuilder.append("You have send a request with a http method not supported.");
-        response.content().setContent(responseBuilder.toString());
-        return true;
+        response.content().setContent("You have send a request with a http method not supported.");
+        response.setStatus(HttpResponseStatus.METHOD_NOT_ALLOWED);
+        return false;
     }
 }
